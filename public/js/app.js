a9d03b363e83b8cebe9dda9ae122944c9ad217e9
Matthew Tully <matthew.j.tully@gmail.com>
2011-07-05 10:24:23 -0400

    slideshow added

--- public/js/app.js
+++ public/js/app.js
@@ -1,41 +1,10 @@
-/* 
-* Skeleton V1.0.2
-* Copyright 2011, Dave Gamache
-* www.getskeleton.com
-* Free to use under the MIT license.
-* http://www.opensource.org/licenses/mit-license.php
-* 5/20/2011
-*/	
-	
-
 $(document).ready(function() {
 
-	/* Tabs Activiation
-	================================================== */
-	var tabs = $('ul.tabs');
-	
-	tabs.each(function(i) {
-		//Get all tabs
-		var tab = $(this).find('> li > a');
-		tab.click(function(e) {
-			
-			//Get Location of tab's content
-			var contentLocation = $(this).attr('href') + "Tab";
-			
-			//Let go if not a hashed one
-			if(contentLocation.charAt(0)=="#") {
-			
-				e.preventDefault();
-			
-				//Make Tab Active
-				tab.removeClass('active');
-				$(this).addClass('active');
-				
-				//Show Tab Content & add active class
-				$(contentLocation).show().addClass('active').siblings().hide().removeClass('active');
-				
-			} 
-		});
-	}); 
+	$('.slide img:gt(0)').hide();
+    setInterval(function(){
+      $('.slide :first-child').fadeOut(2000)
+         .next('img').fadeIn()
+         .end().appendTo('.slide');}, 
+      4000);
 	
 });
